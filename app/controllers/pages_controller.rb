8dbd81a9cdf9a2984f976c88b279da88886bb51d
Maxim Veksler <maxim@vekslers.org>
2016-07-23 21:58:32 +0300

    letsencrypt horimshavim.co.il

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
                           'e4SADLNXLJCdgwlDNNEfDBQOw5eqFIayCqWxzZLwG8I.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when 'H8-gnqcEHLM-PMSWjSpbkkB0L9NN7m03EtBNrmIqhJg'
                           'H8-gnqcEHLM-PMSWjSpbkkB0L9NN7m03EtBNrmIqhJg.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
+                        when '_S6_jFPD2dFnb1V3zhcse3Ld134IGo5n5TFAVkuJtRU'
+                          '_S6_jFPD2dFnb1V3zhcse3Ld134IGo5n5TFAVkuJtRU.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when 'test'
                           'test 123'
                       end
