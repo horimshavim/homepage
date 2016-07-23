187232bc6834a13dfbdb729033086e433eb60267
Maxim Veksler <maxim@vekslers.org>
2016-07-23 22:24:14 +0300

    letsencrypt www.horimshavim.co.il

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
                           'Z0Z6mUV3fYpI2-tEai_8Z9LTgyBtSVrXmVmYj3U7Ez8.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when 'QZnNLwRQOybXi4XS0tfKxXHyfls3usjN_l690b7BpgA'
                           'QZnNLwRQOybXi4XS0tfKxXHyfls3usjN_l690b7BpgA.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
-                        when '_S6_jFPD2dFnb1V3zhcse3Ld134IGo5n5TFAVkuJtRU'
-                          '_S6_jFPD2dFnb1V3zhcse3Ld134IGo5n5TFAVkuJtRU.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
+                        when 'B3BxoztzfoZqgX85MqUEKV2cmduzQs6BQfc9jUEiSxY'
+                          'B3BxoztzfoZqgX85MqUEKV2cmduzQs6BQfc9jUEiSxY.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when 'test'
                           'test 123'
                       end
