1566780206448695f8568ae0d512c0140477c93c
Maxim Veksler <maxim@vekslers.org>
2016-07-23 21:49:46 +0300

    letsencrypt horimshavim.co.il

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "e4SADLNXLJCdgwlDNNEfDBQOw5eqFIayCqWxzZLwG8I.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA"
+    letsencrypt_key = case params[:id]
+                        when 'OPBQAVZrXmO4wLOvqQ1501724HHZetXxfVpzJk1VPZw'
+                          'e4SADLNXLJCdgwlDNNEfDBQOw5eqFIayCqWxzZLwG8I.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
+                        when 'test'
+                          'test 123'
+                      end
+    puts letsencrypt_key
+
+    render plain: letsencrypt_key
   end
 end
