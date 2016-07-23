5b27c5d7079dfc35393c9daf823cdfdb413aecbd
Maxim Veksler <maxim@vekslers.org>
2016-07-23 22:21:46 +0300

    letsencrypt horimshavim.co.il

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def letsencrypt
     letsencrypt_key = case params[:id]
-                        when 'OPBQAVZrXmO4wLOvqQ1501724HHZetXxfVpzJk1VPZw'
-                          'e4SADLNXLJCdgwlDNNEfDBQOw5eqFIayCqWxzZLwG8I.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
+                        when 'Z0Z6mUV3fYpI2-tEai_8Z9LTgyBtSVrXmVmYj3U7Ez8'
+                          'Z0Z6mUV3fYpI2-tEai_8Z9LTgyBtSVrXmVmYj3U7Ez8.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when 'H8-gnqcEHLM-PMSWjSpbkkB0L9NN7m03EtBNrmIqhJg'
                           'H8-gnqcEHLM-PMSWjSpbkkB0L9NN7m03EtBNrmIqhJg.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA'
                         when '_S6_jFPD2dFnb1V3zhcse3Ld134IGo5n5TFAVkuJtRU'
