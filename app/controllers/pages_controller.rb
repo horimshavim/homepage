addd587d0dd7a2f5a3d9e56c8c3c2c34d3462506
Maxim Veksler <maxim@vekslers.org>
2016-07-23 20:42:21 +0300

    Lets encrypt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def letsencrypt
+    render text: "e4SADLNXLJCdgwlDNNEfDBQOw5eqFIayCqWxzZLwG8I.n3edaJv_QDgwWUG5qkM5KJw1daHqdt2fLx4u-Jn1jcA"
+  end
+end
