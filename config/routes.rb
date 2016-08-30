7733791913d8760cae4b283e1f995e6719b253eb
Maxim Veksler <maxim@vekslers.org>
2016-08-30 15:37:09 +0300

    Add better /join

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/press',            to: 'press#index'
   get '/politics',        to: 'politics#index'
   get '/about',            to: 'about#index'
+  get '/join',            to: 'join#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
 end
