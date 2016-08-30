6e897a12e629f3e1d04ef2428146f2764d5b684c
Maxim Veksler <maxim@vekslers.org>
2016-08-30 19:26:57 +0300

    website content
    Support Page
    Drop join links from sub pages
    /join route from main page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/politics',        to: 'politics#index'
   get '/about',            to: 'about#index'
   get '/join',            to: 'join#index'
+  get '/support',            to: 'support#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
 end
